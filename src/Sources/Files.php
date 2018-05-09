31b79b8a9572a3d9582f40289156f42788507b08
paramtamtam <slashkot@gmail.com>
2018-05-09 13:42:53 +0500

    Added connecttions sorting

--- src/Sources/Files.php
+++ src/Sources/Files.php
@@ -76,9 +76,13 @@ class Files implements SourceContract
      */
     public function connections()
     {
-        return array_map(function ($directory_path) {
+        $result = array_sort(array_map(function ($directory_path) {
             return basename($directory_path);
-        }, $this->files->directories($this->migrations_path));
+        }, $this->files->directories($this->migrations_path)));
+
+        sort($result, SORT_NATURAL);
+
+        return $result;
     }
 
     /**
